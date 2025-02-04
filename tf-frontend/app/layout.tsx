a85adc854f996edec32bdba8d88a0d5ae8a9c864
Rob C <rob@trainfair.io>
2025-02-04 15:22:58 -0500

    Added Vercel analytics to app/layout.tsx

--- tf-frontend/app/layout.tsx
+++ tf-frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({
   variable: "--font-sans",
@@ -23,7 +24,10 @@ export default function RootLayout({
         className={`${inter.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
 }
+
+
